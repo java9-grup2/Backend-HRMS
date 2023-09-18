8ff80fcd41b077787f865b261f604de600be0afe
gulsufindik <findik.gulsu@gmail.com>
2023-09-18 21:27:55 +0300

    admin onayi metodu yazildi

--- user-service/src/main/java/org/hrms/constant/EndPoints.java
+++ user-service/src/main/java/org/hrms/constant/EndPoints.java
@@ -11,7 +11,7 @@ public class EndPoints {
     public static final String SAVEMANAGER ="/savemanager";
     public static final String SAVEEMPLOYEE ="/saveemployee";
     public static final String ACTIVATESTATUS ="/activatestatus";
-
+    public static final String APPROVEMANAGER ="/approvemanager";
     public static final String UPDATE ="/update";
     public static final String FINDALL ="/findall";
 
